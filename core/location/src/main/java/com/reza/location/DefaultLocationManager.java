e6d0ea6c34b291e2784d858c309e9dd7c0dc357f
Mohammadreza Rahmani <rahhmani.mohammadreza@gmail.com>
2024-08-29 06:51:38 +0100

    working on emitting periodic location updates

--- core/location/src/main/java/com/reza/location/DefaultLocationManager.java
+++ core/location/src/main/java/com/reza/location/DefaultLocationManager.java
@@ -7,17 +7,21 @@ import android.content.pm.PackageManager;
 import android.location.Location;
 import android.os.Build;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.RequiresApi;
 import androidx.core.app.ActivityCompat;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
 import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.location.Priority;
 
 import javax.inject.Inject;
 import javax.inject.Singleton;
 
+import io.reactivex.BackpressureStrategy;
 import io.reactivex.Flowable;
 import io.reactivex.Single;
 
@@ -52,14 +56,64 @@ public class DefaultLocationManager implements LocationManager {
         });
     }
 
-    private void createLocationRequest() {
-        LocationRequest locationRequest = new LocationRequest.Builder(Priority.PRIORITY_HIGH_ACCURACY, 5000)
+    @NonNull
+    private LocationRequest createLocationRequest() {
+        return new LocationRequest.Builder(Priority.PRIORITY_HIGH_ACCURACY, 5000)
+                .setMinUpdateIntervalMillis(1000)
                 .build();
     }
 
     @RequiresApi(api = Build.VERSION_CODES.S)
     @Override
     public Flowable<Location> getLocationUpdates() {
+        return Flowable.create(emitter -> {
+            if (ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION)
+                    != PackageManager.PERMISSION_GRANTED) {
+                emitter.onError(new Exception(context.getString(R.string.permission_is_not_granted)));
+            } else {
+                LocationCallback locationCallback = new LocationCallback() {
+                    @Override
+                    public void onLocationResult(LocationResult locationResult) {
+                        if (locationResult == null) {
+                            return;
+                        }
+                        for (Location location : locationResult.getLocations()) {
+                            emitter.onNext(location);
+                        }
+                    }
+                };
+            }
+        }, BackpressureStrategy.BUFFER);
+        LocationCallback locationCallback = new LocationCallback() {
+            @Override
+            public void onLocationResult(LocationResult locationResult) {
+                if (locationResult == null) {
+                    return;
+                }
+                for (Location location : locationResult.getLocations()) {
+                    // Update UI with location data
+                    // ...
+                }
+
+                // todo emit location values here
+            }
+        };
+        if (ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return TODO;
+        } else {
+
+        }
+        fusedLocationClient.requestLocationUpdates(new LocationRequest.Builder(Priority.PRIORITY_HIGH_ACCURACY, 5000)
+                        .setMinUpdateIntervalMillis(1000)
+                        .build(),
+                locationCallback, null);
         final long timeInterval = 5000;
         return Flowable.just(new Location(""));
     }
